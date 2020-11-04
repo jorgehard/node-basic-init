6e739bc1b83c9455cae95e0548b4a4e2be2a8f18
Jorge H Baptista <jorgehenrique@live.com>
2020-11-04 07:18:24 -0300

    Projeto Base

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.post('/user', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  response.json(user);
+});
+export default routes;
